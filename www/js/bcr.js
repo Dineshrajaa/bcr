ad4137c42f215d8becc50c7cbd8938805cd5a4ed
Dineshrajaa <dineshrajaa.93@gmail.com>
2014-12-18 15:12:41 +0530

    Added Toast Alert

--- www/js/bcr.js
+++ www/js/bcr.js
@@ -1,19 +1,23 @@
 $(document).ready(function(){
 
 	document.addEventListener('deviceready',function(){
-		$("#scanbtn").click(startScan);
+		$("#scanbtn").tap(startScan);//Calls startScan method when scan button tapped
 			
 		function startScan(){
-			cordova.plugins.barcodeScanner.scan(onScan,onErrors);
+			cordova.plugins.barcodeScanner.scan(onScan,onErrors);//Opens  the Camera to scan the CODE and display the 
 		}
 		function onScan(results){
-			navigator.notification.alert(results.text,alertClosed,results.format,'ok');			
+			navigator.notification.alert(results.text,alertClosed,results.format,'ok');//Displays an Custom alertbox			
 		}
 		function onErrors(error){
-			alert("Scanning Failed: "+error);		
-					 		}
+			toastAlert("Scanning Failed: "+error);		
+					 		}//Called when the Barcode scanner is not properly Functioning
 		function alertClosed(){			
-			alert("Thanks for using BCR");
+			toastAlert("Thanks for using BCR");
+		}
+		//Used to create Toast Alerts
+		function toastAlert(data){
+			window.plugins.toast.showLongCenter(data);
 		}
 	});
 });
