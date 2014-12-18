4a017e5bc69c194a6147613ef73d3e554032dcf4
Dineshrajaa <dineshrajaa.93@gmail.com>
2014-12-18 12:24:45 +0530

    Second build

--- www/js/bcr.js
+++ www/js/bcr.js
@@ -8,11 +8,14 @@ $(document).ready(function(){
 		}
 		function onScan(results){
 			var op="Content: "+results.text+"<br />"+
-			"Format: "+results.format+"<br />"
+			"Format: "+results.format;
 			$("#outputdiv").html(op);
 		}
 		function onErrors(error){
 			alert("Scanning Failed: "+error);
+			var fop="Content: Not Found<br />
+					 Format: Unable to detect"
+					 $("#outputdiv").html(fop);
 		}
 	});
 });
